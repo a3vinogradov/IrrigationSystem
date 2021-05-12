557ebfccda28cca2dbf270f241786e8a6658747c
Vinogradov Alexander <a3vinogradov@gmail.com>
2021-05-12 22:47:18 +0300

    Job-007 Добавить датчик DHT22

--- Arduino/Irrigation/Irrigation.ino
+++ Arduino/Irrigation/Irrigation.ino
@@ -2,6 +2,7 @@
 //#include <Adafruit_GFX.h>
 //#include <Adafruit_PCD8544.h>
 #include <LiquidCrystal.h>
+#include "DHT.h"
 
 // вывод управления реле
 #define releyPin 5
@@ -21,54 +22,102 @@
 
 // кнопка принудительного включения насоса
 #define buttonPin 4
+#define DHT22Pin 3
+#define DHTTYPE DHT22 
 
+// создание экземпляра объекта сенсора влажности почвы
 MHSensor Sensor(sensorPower, sensorPin);
+
+// создание экземпляра объекта дисплея
 LiquidCrystal lcd(DisplayRS,DisplayE,DisplayDB4,DisplayDB5,DisplayDB6, DisplayDB7);
+
+// создание экземпляра объекта DHT
+DHT dht(DHT22Pin, DHTTYPE);
+
 int MoistreMax = 0;
 int PompCount = 0;
 int MoistreVal = 0;
+int HumidityVal = 0;
+int TemperatureVal = 0;
+
 void setup(){ 
   MoistreMax = 700;
   lcd.begin(16, 2);
   pinMode(releyPin, OUTPUT);
   MoistreVal = Sensor.GetValue();
+  dht.begin();
 }
 
 void loop() 
 {
-  for(int i=0; i<20; i++){  
+  for(int i=0; i<40; i++){  
     int butVal = !digitalRead(buttonPin);
     digitalWrite(releyPin, butVal);
     MoistreMax = 1024 - analogRead(MoistreSetupPin);
     
-    DrawDisplay(MoistreVal, MoistreMax, butVal, PompCount);
+    DrawDisplay(MoistreVal, MoistreMax, PompCount, TemperatureVal, HumidityVal);
     delay(100);
   }
 
+  HumidityVal = dht.readHumidity();
+  TemperatureVal = dht.readTemperature();
   MoistreVal = Sensor.GetValue();
   if (MoistreVal > MoistreMax){
     PompCount++;
-    DrawDisplay(MoistreVal, MoistreMax, 1, PompCount);
     digitalWrite(releyPin, HIGH);
-    delay(8000);
-    DrawDisplay(MoistreVal, MoistreMax, 0, PompCount);
+    for(int i=0; i<8; i++){
+      DrawDisplayIrrigation(i, 8);
+      delay(1000);
+    }
+    //DrawDisplay(MoistreVal, MoistreMax, 0, PompCount);
     digitalWrite(releyPin, LOW);
-    delay(10000); 
-   }
+    for(int i=0; i<10; i++){
+      DrawDisplayWaiting(i, 10);
+      delay(1000);
+    }
+    DrawDisplay(MoistreVal, MoistreMax, PompCount, TemperatureVal, HumidityVal);     
+  }
 }
-void DrawDisplay(int mhVal, int mhMax, int relBut, int pompCount){
+void DrawDisplay(int mhVal, int mhMax, int pompCount, int temp, int hum){
   lcd.clear();
   // 1 строка
   lcd.setCursor(0, 0);
-  lcd.print("Mstr: ");
+  lcd.print("Mst:");
   lcd.print(mhVal);
   lcd.print("/");
   lcd.print(mhMax);
-  
-  lcd.setCursor(0, 1);
-  lcd.print("Pomp: ");
-  lcd.print((relBut?"ON ":"OFF"));
-  lcd.print(" (");
+  lcd.print(" {");
   lcd.print(pompCount);
   lcd.print("}");
+  // 2 строка
+  lcd.setCursor(0, 1);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print(" Hum: ");
+  lcd.print(hum);
+}
+
+void DrawDisplayIrrigation(int curDelay, int maxDelay)
+{
+  lcd.clear();
+  // 1 строка
+  lcd.setCursor(0, 0);
+  lcd.print("Pomp ON");
+  // 2 строка
+  lcd.setCursor(0, 1);
+  lcd.print("Waiting ");
+  lcd.print(maxDelay-curDelay);
+  lcd.print(" second");
+}
+void DrawDisplayWaiting(int curDelay, int maxDelay)
+{
+  lcd.clear();
+  // 1 строка
+  lcd.setCursor(0, 0);
+  lcd.print("Pomp OFF");
+  // 2 строка
+  lcd.setCursor(0, 1);
+  lcd.print("Waiting ");
+  lcd.print(maxDelay-curDelay);
+  lcd.print(" second");
 }
