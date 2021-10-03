ec3d8d38bceb91a56f4aa6982d077059e6fc3f67
Vinogradov Alexander <a3vinogradov@gmail.com>
2021-10-03 14:56:53 +0300

    Job-008 Сделать "метеостанцию" на отдельной сборке Показания с DHT11 и BMP280 отправляются на народмон

--- Arduino/MeteoStationESP/MeteoStationESP.ino
+++ Arduino/MeteoStationESP/MeteoStationESP.ino
@@ -1,29 +1,73 @@
+// "Метеостанция" на столе: 
+// NodeMCU v1
+// bmp280 (D1->SCL; D2->SDA; Gnd; Vcc)
+// dht11 (D5->S; Vcc; Gnd)
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 Adafruit_BMP280 bmp280;
 
 #include "DHT.h"
-DHT dht(D3, DHT11);
+DHT dht(D5, DHT11);
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+ESP8266WiFiMulti WiFiMulti;
+
+  double temperature1 = 0; 
+  double pressure = 0; 
+  double humidity = 0;
+  double temperature2 = 0; 
 
 void setup()
 {
-  Serial.begin(9600); 
-
-  if (!bmp280.begin(0x76)) {   
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);  
+  Serial.begin(115200); 
+  SetupSensors();
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
   }
-  dht.begin();
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("BelkoNet", "liskabelko");
+
+  //SentMessage();
 }
 
 void loop()
 {
+  ReadSensors();
+  PrintSensors();
 
-  double temperature1 = bmp280.readTemperature(); 
-  double pressure = bmp280.readPressure(); 
-  double humidity = dht.readHumidity();
-  double temperature2 = dht.readTemperature(); 
+  SentMessage();
+  Serial.println("\n =============================== \n");
+      
+  delay(600000);
+  
+}
 
+void SetupSensors()
+{
+  // D1 -> SCL
+  // D2 -> SDA
+  if (!bmp280.begin(0x76)) {   
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+  }
+  dht.begin();
+}
+void ReadSensors()
+{
+  temperature1 = bmp280.readTemperature(); 
+  pressure = bmp280.readPressure(); 
+  humidity = dht.readHumidity();
+  temperature2 = dht.readTemperature();   
+}
+void PrintSensors()
+{
   Serial.print("T(bmp) = ");
   Serial.print(temperature1);
   Serial.println("*C");
@@ -36,10 +80,52 @@ void loop()
   Serial.print("H      = ");
   Serial.print(humidity);
   Serial.println("%");
-  
-  Serial.println("\n =============================== \n");
-  
-  
-  delay(5000);
-  
+}
+
+void SentMessage()
+{
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    WiFiClient client;
+
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    http.begin(client, "http://narodmon.ru/json");
+    http.addHeader("Content-Type", "application/json");
+    
+    String Message = String("{ \"devices\":[{\"mac\": \"14DE803A90C9\",\"sensors\":[{ \"id\": \"Temperature1\",\"value\": ") +
+          temperature1 +
+          String("},{\"id\": \"Pressure1\",\"value\": ")+
+          pressure +
+          String("},{\"id\": \"Humidity1\",\"value\": ")+
+          humidity +
+          String("}]}]}");
+    
+    Serial.print("[HTTP] POST...\n");
+
+    
+    // start connection and send HTTP header and body
+    //int httpCode = http.POST("{ \"devices\":[{\"mac\": \"14DE803A90C9\",\"sensors\":[{ \"id\": \"Temperature1\",\"value\": 20},{\"id\": \"Pressure1\",\"value\": 23},{\"id\": \"Humidity1\",\"value\": 23}]}]}");
+    int httpCode = http.POST(Message);
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+      http.end();
+  }  
 }
